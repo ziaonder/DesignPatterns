e8a482fd2d3ab2ac315bddc09e42a8a24fc16b09
ziaonder <zia.info1@gmail.com>
2024-12-30 23:36:24 +0300

    Singleton added

---
+++ Singleton/Program.cs
@@ -0,0 +1,9 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        //Singleton singleton = Singleton.Instance;
+        //singleton.DoSome();
+        Singleton.Instance.DoSome();
+    }
+}
---
+++ Singleton/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Singleton
+{
+    private static Singleton instance;
+
+    private Singleton()
+    {
+        Console.WriteLine("Singleton created");
+    }
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new Singleton();
+            }
+
+            return instance;
+        }
+    }
+
+    public void DoSome()
+    {
+        Console.WriteLine("hey from singleton");
+    }
+}
